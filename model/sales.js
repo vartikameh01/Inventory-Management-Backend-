785439d411683fb01a8b931485c45a5b37b340b8
vishalsaraswatcs23-sudo <vishal.saraswat_cs23@gla.ac.in>
2026-04-16 13:48:31 +0530

    model

---
+++ model/sales.js
@@ -0,0 +1,18 @@
+import mongoose from "../config/connection.db.js";
+
+// Sale Schema
+const saleSchema = new mongoose.Schema({
+    product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product"
+    },
+    quantity: Number,
+    price: Number,
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+// Export Model
+export default mongoose.model("Sale", saleSchema);
